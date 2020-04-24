ede3c92c03d4f843ced3e1bce3ee193a38336b86
Web User <webuser@www.helpinout.org>
2020-04-24 18:48:34 +0530

    Delete config/prod.php with sensitive production details from repo

--- config/prod.php
+++ config/prod.php
@@ -4,13 +4,12 @@ $custome_array = (array_replace_recursive(
                 require(dirname(__FILE__) . '/config.php'), array(
             'components' => [
                 'db' => [
-                    'dsn' => 'hio-aurora-mysql-mumbai-1.cluster-cx5te3q4gasc.ap-south-1.rds.amazonaws.com;dbname=helpinout',
+                    'dsn' => 'mysql:host=hio-aurora-mysql-mumbai-1.cluster-cx5te3q4gasc.ap-south-1.rds.amazonaws.com;dbname=helpinout',
                     'username' => 'helpinout',
                     'password' => 'Phoht0eeshaeha',
                 ],
               ]
-            ],
-                )
+           )
 	));
 
 return $custome_array;
